1352adb07f88ddd2140632075449bc48a158900a
Digo <148949668+MohamedElDegwi@users.noreply.github.com>
2023-12-28 01:52:58 +0200

    Added Double-LinkedList ADT

--- Data Structures & Algorithms Practice Project/Data Structures & Algorithms Practice Project.cpp
+++ Data Structures & Algorithms Practice Project/Data Structures & Algorithms Practice Project.cpp
@@ -1,20 +1,49 @@
-// Data Structures & Algorithms Practice Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "clsDblLinkedList.h"
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    clsDblLinkedList <int> MydblLinkedList;
+
+    MydblLinkedList.InsertAtBeginning(5);
+    MydblLinkedList.InsertAtBeginning(4);
+    MydblLinkedList.InsertAtBeginning(3);
+    MydblLinkedList.InsertAtBeginning(2);
+    MydblLinkedList.InsertAtBeginning(1);
+
+    std::cout << "\nLinked List Contenet:\n";
+    MydblLinkedList.PrintList();
+
+    clsDblLinkedList<int>::Node* N1 = MydblLinkedList.Find(2);
+
+    if (N1 != NULL)
+        std::cout << "\nNode with value 2 is Found :-)\n";
+    else
+        std::cout << "\nNode Is not found :-(\n";
+
+    MydblLinkedList.InsertAfter(N1, 500);
+    std::cout << "\nAfter Inserting 500 after 2:\n";
+    MydblLinkedList.PrintList();
+
+    MydblLinkedList.InsertAtEnd(700);
+    std::cout << "\nAfter Inserting 700 at end:\n";
+    MydblLinkedList.PrintList();
+
+    clsDblLinkedList<int>::Node* N2 = MydblLinkedList.Find(4);
+    MydblLinkedList.DeleteNode(N2);
+    std::cout << "\nAfter Deleting 4:\n";
+    MydblLinkedList.PrintList();
+
+    MydblLinkedList.DeleteFirstNode();
+    std::cout << "\nAfter Deleting First Node:\n";
+    MydblLinkedList.PrintList();
+
+    std::cout << "\nAfter Deleting Last Node:\n";
+    MydblLinkedList.DeleteLastNode();
+    MydblLinkedList.PrintList();
+
+    system("pause>0");
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
